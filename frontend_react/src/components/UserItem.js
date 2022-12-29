924d30efc1301a1e6ea9935440d1d26dae7fd1f6
AaYaqin25 <ahmadainulyaqin777@gmail.com>
2022-12-29 16:46:58 +0700

    Done All

--- frontend_react/src/components/UserItem.js
+++ frontend_react/src/components/UserItem.js
@@ -1,16 +1,20 @@
-import React, { Fragment, useState } from "react"
+import React, { Fragment, useCallback, useState } from "react"
 
 export default function UserItem(props) {
 
     const [user, setUser] = useState({
         name: props.name,
         phone: props.phone,
+
+    })
+
+    const [edit, setEdit] = useState({
         isEdit: false
     })
 
     const handleInputChange = (event) => {
         const target = event.target;
-        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const value = target.value;
         const name = target.name;
 
         setUser({
@@ -20,31 +24,33 @@ export default function UserItem(props) {
     }
 
     const handleClickEdit = () => {
-        setUser({
+        setEdit({
             isEdit: true
         })
 
     }
 
     const handleCancelEdit = () => {
-        setUser({
+        setEdit({
             isEdit: false
         })
     }
 
-    const saveEdit = () => {
+    const saveEdit = useCallback(() => {
         props.update(user.name, user.phone)
-        setUser({
+        setEdit({
             isEdit: false
         })
-    }
-   
-    console.log(user.name);
+    }, [props, user])
+
+    console.log(props.name, 'ini name');
+    console.log(props.phone, 'ini phone');
+    console.log(user.isEdit, 'mode edit');
     return (
         <tr>
             <td>{props.no}</td>
             {
-                user.isEdit ?
+                edit.isEdit ?
                     <Fragment>
                         <td>
                             <input type="text" id="name" name='name' className="form-control" placeholder='name' onChange={handleInputChange} value={user.name} />
@@ -62,12 +68,13 @@ export default function UserItem(props) {
 
             {
                 props.sent ?
-                    user.isEdit ?
+                    edit.isEdit ?
                         <td>
                             <button className='btn btn-info' onClick={saveEdit}><i className="fa-sharp fa-solid fa-pen"></i> save</button>
                             <button className='btn btn-warning' onClick={handleCancelEdit}><i className="fa-solid fa-ban"></i> cancel</button>
                         </td>
-                        : <td>
+                        :
+                        <td>
                             <button id='btnedit' className='btn btn-light' onClick={handleClickEdit}><i className="fa-sharp fa-solid fa-pen"></i> edit</button>
                             <button id='btndelete' className='btn btn-light' onClick={props.remove}><i className="fa-solid fa-ban"></i> delete</button>
                         </td>
